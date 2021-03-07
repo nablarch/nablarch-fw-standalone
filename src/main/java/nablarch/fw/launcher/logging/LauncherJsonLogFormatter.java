653bfdc5fdd968b837dce212e9f26a8f3b886e7d
shuji-k <kitamura.shuji@tis.co.jp>
2021-03-08 02:25:32 +0900

    feat: JSON版の起動ログフォーマッタを追加

---
+++ src/main/java/nablarch/fw/launcher/logging/LauncherJsonLogFormatter.java
@@ -0,0 +1,127 @@
+package nablarch.fw.launcher.logging;
+
+import nablarch.core.log.app.AppLogUtil;
+import nablarch.core.log.app.JsonLogFormatterSupport;
+import nablarch.core.util.StringUtil;
+import nablarch.fw.launcher.CommandLine;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 起動ログのメッセージをJSON形式でフォーマットするクラス。
+ *
+ * @author Shuji Kitamura
+ */
+public class LauncherJsonLogFormatter extends LauncherLogFormatter {
+
+    /** 連絡先の項目名 */
+    private static final String TARGET_NAME_COMMAND_LINE_OPTIONS = "commandLineOptions";
+    /** 連絡先の項目名 */
+    private static final String TARGET_NAME_COMMAND_LINE_ARGUMENTS = "commandLineArguments";
+    /** 連絡先の項目名 */
+    private static final String TARGET_NAME_EXIT_CODE = "exitCode";
+    /** 連絡先の項目名 */
+    private static final String TARGET_NAME_EXECUTE_TIME = "executeTime";
+
+    /** 開始ログの出力項目を取得する際に使用するプロパティ名 */
+    private static final String PROPS_START_LOGTARGETS = PROPS_PREFIX + "startTargets";
+    /** 終了ログの出力項目を取得する際に使用するプロパティ名 */
+    private static final String PROPS_END_LOG_TARGETS = PROPS_PREFIX + "endTargets";
+
+    /** 開始ログ出力項目のデフォルト値 */
+    private static final String DEFAULT_START_LOG_TARGETS = "commandLineOptions,commandLineArguments";
+    /** 終了ログ出力項目のデフォルト値 */
+    private static final String DEFAULT_END_LOG_TARGETS = "exitCode,executeTime";
+
+    /** 各種ログのJSONフォーマット支援オブジェクト */
+    private JsonLogFormatterSupport support = null;
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getStartLogMsg(CommandLine commandLine) {
+        if (support == null) support = new JsonLogFormatterSupport(PROPS_PREFIX);
+
+        Map<String, Object> structuredObject
+                = getStartStructuredObject(AppLogUtil.getProps(), commandLine);
+        return support.getStructuredMessage(structuredObject);
+    }
+
+    /**
+     * 開始ログの構造化オブジェクトを生成する。
+     * @param props 各種ログ出力の設定情報
+     * @param commandLine {@link CommandLine コマンドラインオブジェクト}
+     * @return 構造化オブジェクト
+     */
+    protected Map<String, Object> getStartStructuredObject(Map<String, String> props, CommandLine commandLine) {
+        String targetsStr = props.get(PROPS_START_LOGTARGETS);
+        if (StringUtil.isNullOrEmpty(targetsStr)) targetsStr = DEFAULT_START_LOG_TARGETS;
+
+        Map<String, Object> structuredObject = new HashMap<String, Object>();
+
+        String[] targets = targetsStr.split(",");
+        Set<String> keys = new HashSet<String>(targets.length);
+        for (String target: targets) {
+            String key = target.trim();
+            if (!StringUtil.isNullOrEmpty(key) && !keys.contains(key)) {
+                keys.add(key);
+                if (TARGET_NAME_COMMAND_LINE_OPTIONS.equals(key)) {
+                    structuredObject.put(
+                            TARGET_NAME_COMMAND_LINE_OPTIONS,
+                            commandLine.getParamMap());
+                } else if (TARGET_NAME_COMMAND_LINE_ARGUMENTS.equals(key)) {
+                    structuredObject.put(
+                            TARGET_NAME_COMMAND_LINE_ARGUMENTS,
+                            commandLine.getArgs());
+                }
+            }
+        }
+
+        return structuredObject;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getEndLogMsg(int exitCode, long executeTime) {
+        if (support == null) support = new JsonLogFormatterSupport(PROPS_PREFIX);
+
+        Map<String, Object> structuredObject
+                = getEndStructuredObject(AppLogUtil.getProps(), exitCode, executeTime);
+        return support.getStructuredMessage(structuredObject);
+    }
+
+    /**
+     * 終了ログの構造化オブジェクトを生成する。
+     * @param props 各種ログ出力の設定情報
+     * @param exitCode 終了コード
+     * @param executeTime 処理時間
+     * @return 構造化オブジェクト
+     */
+    protected Map<String, Object> getEndStructuredObject(Map<String, String> props, int exitCode, long executeTime) {
+        String targetsStr = props.get(PROPS_END_LOG_TARGETS);
+        if (StringUtil.isNullOrEmpty(targetsStr)) targetsStr = DEFAULT_END_LOG_TARGETS;
+
+        Map<String, Object> structuredObject = new HashMap<String, Object>();
+
+        String[] targets = targetsStr.split(",");
+        Set<String> keys = new HashSet<String>(targets.length);
+        for (String target: targets) {
+            String key = target.trim();
+            if (!StringUtil.isNullOrEmpty(key) && !keys.contains(key)) {
+                keys.add(key);
+                if (TARGET_NAME_EXIT_CODE.equals(key)) {
+                    structuredObject.put(TARGET_NAME_EXIT_CODE, exitCode);
+                } else if (TARGET_NAME_EXECUTE_TIME.equals(key)) {
+                    structuredObject.put(TARGET_NAME_EXECUTE_TIME, executeTime);
+                }
+            }
+        }
+
+        return structuredObject;
+    }
+
+}
---
+++ src/test/java/nablarch/fw/launcher/logging/LauncherJsonLogFormatterTest.java
@@ -0,0 +1,163 @@
+package nablarch.fw.launcher.logging;
+
+import nablarch.core.log.LogTestSupport;
+import nablarch.fw.launcher.CommandLine;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * {@link nablarch.fw.launcher.logging.LauncherJsonLogFormatter}のテストクラス。
+ *
+ * @author Shuji Kitamura
+ */
+public class LauncherJsonLogFormatterTest extends LogTestSupport {
+
+    @Before
+    public void setup() {
+        System.clearProperty("performanceLogFormatter.targets");
+    }
+
+    @After
+    public void teardown() {
+        System.clearProperty("performanceLogFormatter.targets");
+    }
+
+    /**
+     * {@link LauncherJsonLogFormatter#getStartLogMsg(CommandLine)}のテスト。
+     * デフォルトの出力項目で正しくフォーマットされること。
+     */
+    @Test
+    public void testGetStartLogMsg() {
+
+        LauncherLogFormatter formatter = new LauncherJsonLogFormatter();
+
+        String requestPath = "nablarch.hoge.HogeAction/RBHOGEHOGE";
+        String userId = "testUser";
+        String diConfig = "test.xml";
+
+        CommandLine commandLine = new CommandLine(
+                "-diConfig", diConfig,
+                "-userId", userId,
+                "-requestPath", requestPath
+        );
+
+        String message = formatter.getStartLogMsg(commandLine);
+        assertThat(message.startsWith("$JSON$"), is(true));
+        assertThat(message.substring(6), isJson(allOf(
+                withJsonPath("$.commandLineOptions", hasEntry("diConfig", "test.xml")),
+                withJsonPath("$.commandLineOptions", hasEntry("userId", "testUser")),
+                withJsonPath("$.commandLineOptions", hasEntry("requestPath", "nablarch.hoge.HogeAction/RBHOGEHOGE")),
+                withJsonPath("$.commandLineArguments", hasSize(0)))));
+
+        commandLine = new CommandLine(
+                "-diConfig", diConfig,
+                "-userId", userId,
+                "-requestPath", requestPath,
+                "-param1", "param1",
+                "-param2", "param2",
+                "h", "o", "g", "e", "hoge",
+                "-param3", "param3",
+                "fuga"
+        );
+
+        message = formatter.getStartLogMsg(commandLine);
+        assertThat(message.startsWith("$JSON$"), is(true));
+        assertThat(message.substring(6), isJson(allOf(
+                withJsonPath("$.commandLineOptions", hasEntry("diConfig", "test.xml")),
+                withJsonPath("$.commandLineOptions", hasEntry("userId", "testUser")),
+                withJsonPath("$.commandLineOptions", hasEntry("requestPath", "nablarch.hoge.HogeAction/RBHOGEHOGE")),
+                withJsonPath("$.commandLineOptions", hasEntry("param1", "param1")),
+                withJsonPath("$.commandLineOptions", hasEntry("param2", "param2")),
+                withJsonPath("$.commandLineOptions", hasEntry("param3", "param3")),
+                withJsonPath("$.commandLineArguments", hasSize(6)),
+                withJsonPath("$.commandLineArguments[0]", is("h")),
+                withJsonPath("$.commandLineArguments[1]", is("o")),
+                withJsonPath("$.commandLineArguments[2]", is("g")),
+                withJsonPath("$.commandLineArguments[3]", is("e")),
+                withJsonPath("$.commandLineArguments[4]", is("hoge")),
+                withJsonPath("$.commandLineArguments[5]", is("fuga")))));
+    }
+
+    /**
+     * {@link LauncherJsonLogFormatter#getStartLogMsg(CommandLine)}のテスト。
+     * 不正なターゲットのテスト。
+     */
+    @Test
+    public void testGetStartLogMsgWithIllegalTargets() {
+        System.setProperty("launcherLogFormatter.startTargets", "commandLineOptions ,, ,dummy,commandLineOptions");
+
+        LauncherLogFormatter formatter = new LauncherJsonLogFormatter();
+
+        String requestPath = "nablarch.hoge.HogeAction/RBHOGEHOGE";
+        String userId = "testUser";
+        String diConfig = "test.xml";
+
+        CommandLine commandLine = new CommandLine(
+                "-diConfig", diConfig,
+                "-userId", userId,
+                "-requestPath", requestPath,
+                "h", "o", "g", "e", "hoge"
+        );
+
+        String message = formatter.getStartLogMsg(commandLine);
+
+        assertThat(message.startsWith("$JSON$"), is(true));
+        assertThat(message.substring(6), isJson(allOf(
+                withJsonPath("$.commandLineOptions", hasEntry("diConfig", "test.xml")),
+                withJsonPath("$.commandLineOptions", hasEntry("userId", "testUser")),
+                withJsonPath("$.commandLineOptions", hasEntry("requestPath", "nablarch.hoge.HogeAction/RBHOGEHOGE")),
+                withoutJsonPath("$.commandLineArguments"))));
+    }
+
+    /**
+     * {@link LauncherJsonLogFormatter#getEndLogMsg(int, long)}のテスト。
+     * デフォルトの出力項目で正しくフォーマットされること。
+     */
+    @Test
+    public void testGetEndLogMsg() {
+
+        LauncherLogFormatter formatter = new LauncherJsonLogFormatter();
+
+        String message = formatter.getEndLogMsg(0, 100);
+        assertThat(message.startsWith("$JSON$"), is(true));
+        assertThat(message.substring(6), isJson(allOf(
+                withJsonPath("$", hasEntry("exitCode", 0)),
+                withJsonPath("$", hasEntry("executeTime", 100)))));
+
+        message = formatter.getEndLogMsg(101, 100000000);
+        assertThat(message.startsWith("$JSON$"), is(true));
+        assertThat(message.substring(6), isJson(allOf(
+                withJsonPath("$", hasEntry("exitCode", 101)),
+                withJsonPath("$", hasEntry("executeTime", 100000000)))));
+
+        message = formatter.getEndLogMsg(1, Long.MAX_VALUE);
+        assertThat(message.startsWith("$JSON$"), is(true));
+        assertThat(message.substring(6), isJson(allOf(
+                withJsonPath("$", hasEntry("exitCode", 1)),
+                withJsonPath("$", hasEntry("executeTime", Long.MAX_VALUE)))));
+    }
+
+    /**
+     * {@link LauncherJsonLogFormatter#getEndLogMsg(int, long)}のテスト。
+     * 不正なターゲットのテスト。
+     */
+    @Test
+    public void testGetEndLogMsgWithIllegalTargets() {
+        System.setProperty("launcherLogFormatter.endTargets", "exitCode ,, ,dummy,exitCode");
+
+        LauncherLogFormatter formatter = new LauncherJsonLogFormatter();
+
+        String message = formatter.getEndLogMsg(0, 100);
+        assertThat(message.startsWith("$JSON$"), is(true));
+        assertThat(message.substring(6), isJson(allOf(
+                withJsonPath("$", hasEntry("exitCode", 0)),
+                withoutJsonPath("$.executeTime"))));
+    }
+
+}
